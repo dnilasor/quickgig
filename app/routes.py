f1f1964de84cbcf01e7784e8635a66a5396a18a8
dnilasor <rosalind26@gmail.com>
2019-10-16 09:08:42 -0700

    require login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template, flash, redirect
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from app import app
+from app import app, db
 from app.forms import LoginForm
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
+
 def index():
   user = {'username': 'Dnilasor'}
   gigs = [
@@ -27,11 +30,14 @@ def login():
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(username=form.username.data).first()
-	if user is None or not user.check_password(form.password.data):
-	  flash('Invalid username or password')
-	  return redirect(url_for('login'))
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
     login_user(user, remember=form.remember_me.data)
-	return redirect(url_for('index'))
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')
+    return redirect(next_page)
   return render_template('login.html', title='Log In', form=form)
   
 @app.route('/logout')
