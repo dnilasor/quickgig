015c9cb953cdae2af79b9722d9f1495b11663faa
dnilasor <rosalind26@gmail.com>
2019-10-17 14:29:44 -0700

    implement last seen

--- app/models.py
+++ app/models.py
@@ -11,6 +11,10 @@ class User(UserMixin, db.Model):
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
   gigs = db.relationship('Gig', backref='employer', lazy='dynamic')
+  about_me = db.Column(db.String(140))
+  first_name = db.Column(db.String(40))
+  last_name = db.Column(db.String(40))
+  last_seen = db.Column(db.DateTime, default=datetime.utcnow)
   
   def __repr__(self):
     return '<User {}>'.format(self.username)
