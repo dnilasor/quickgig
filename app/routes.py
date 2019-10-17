fa963c0965e681c531327c99fb04ae95c902ded4
dnilasor <rosalind26@gmail.com>
2019-10-17 10:41:00 -0700

    basic profile route n template

--- app/routes.py
+++ app/routes.py
@@ -58,3 +58,13 @@ def signup():
     flash('Congrats, you are signed up!')
     return redirect(url_for('login'))
   return render_template('signup.html', title='Signup', form=form)
+  
+  @app.route('/user/<username>')
+  @login_required
+  def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    gigs = [
+	  {'author': user, 'body': 'Test gig #1'},
+      {'author': user, 'body': 'Test gig #1'}
+    ]
+    return render_templare('user.html'. user=user, gigs=gigs)
