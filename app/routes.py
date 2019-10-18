3b01d5b83c6b197ae3efe84996a6c653bf8534fa
dnilasor <rosalind26@gmail.com>
2019-10-17 23:27:09 -0700

    add edit profile function

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from app import app, db
-from app.forms import LoginForm, SignupForm
+from app.forms import LoginForm, SignupForm, EditProfileForm
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -75,3 +75,18 @@ def user(username):
     {'author': user, 'body': 'Test gig #1'}
   ]
   return render_template('user.html', user=user, gigs=gigs)
+  
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+  form = EditProfileForm()
+  if form.validate_on_submit():
+    current_user.username = form.username.data
+    current_user.about_me = form.about_me.data
+    db.session.commit()
+    flash('Your changes have been saved')
+    return redirect(url_for('edit_profile'))
+  elif request.method == 'GET':
+    form.username.data = current_user.username
+    form.about_me.data = current_user.about_me
+  return render_template('edit_profile.html', title='Edit Profile', form=form)
