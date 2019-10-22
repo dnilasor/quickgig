7f994757acaea649881efffdf71857565040de54
dnilasor <rosalind26@gmail.com>
2019-10-22 13:22:57 -0700

    favoriters model changes

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from app import db
 from app import login
 from hashlib import md5
 
+
+favoriters = db.Table('favorites',
+  db.Column('favoriter_id', db.Integer, db.ForeignKey('user.id')),
+  db.Column('favorited_id', db.Integer, db.ForeignKey('user.id'))
+  )
+
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
@@ -15,6 +21,11 @@ class User(UserMixin, db.Model):
   first_name = db.Column(db.String(40))
   last_name = db.Column(db.String(40))
   last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+  favorited = db.relationship(
+    'User', secondary=favoriters,
+	primaryjoin=(favoriters.c.favoriter_id == id),
+    secondaryjoin=(favoriters.c.favorited_id == id),
+    backref=db.backref('favoriters', lazy='dynamic'), lazy='dynamic')
   
   def __repr__(self):
     return '<User {}>'.format(self.username)
@@ -28,6 +39,26 @@ class User(UserMixin, db.Model):
   def avatar(self, size):
     digest = md5(self.email.lower().encode('utf-8')).hexdigest()
     return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)	
+	
+  def favorite(self, user):
+    if not self.is_favorited(user):
+      self.favorited.append(user)
+  
+  def unfavorite(self, user):
+    if self.is_favorited(user):
+      self.favorited.remove(user)
+
+  def is_favorited(self, user):
+    return self.favorited.filter(
+      favoriters.c.favorited_id == user.id).count() > 0
+
+  def favorite_gigs(self):
+    favorited = Gig.query.join(
+      favoriters, (favoriters.c.favorited_id == Gig.user_id.filter(
+        favoriters.c.favoriter_id == self.id)
+    own = Gig.query.filter_by(user_id=self.id)
+    return favorited.union(own).order_by(
+          Gig.timestamp.desc())
 	
 @login.user_loader
 def load_user(id):
