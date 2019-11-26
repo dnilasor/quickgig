59dbf1580a732be350a2c77e36a9d53c467643e4
Jay <jaymeyer2017@u.northwestern.edu>
2019-11-25 22:00:30 -0600

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import ValidationError, DataRequired, Length, InputRequired
 from flask_babel import _, lazy_gettext as _l
-from app.models import User, Gig, Neighborhood
+from app.models import User, Gig, Neighborhood, Gigtype
 
 
 class EditProfileForm(FlaskForm):
@@ -25,6 +25,7 @@ class EditProfileForm(FlaskForm):
 class GigForm(FlaskForm):
   gig = TextAreaField(_l('Describe your gig here'), validators=[DataRequired(), Length(min=1, max=300)])
   neighborhood = QuerySelectField(query_factory=lambda: Neighborhood.query.all(), get_label="name", allow_blank=False)
+  type = QuerySelectField(query_factory=lambda: Gigtype.query.all(), get_label="name", allow_blank=False)
   submit = SubmitField(_l('Submit'))
 
 class SearchForm(FlaskForm):
