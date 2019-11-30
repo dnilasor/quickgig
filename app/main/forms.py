6174bebba681c55b59e2fd61fac1f9e48dbb1a94
Jay <jaymeyer2017@u.northwestern.edu>
2019-11-29 22:47:42 -0600

    Update forms.py
    Start date logic

--- app/main/forms.py
+++ app/main/forms.py
@@ -27,9 +27,10 @@ class GigForm(FlaskForm):
   gig = TextAreaField(_l('Describe your gig here'), validators=[DataRequired(), Length(min=1, max=300)])
   neighborhood = QuerySelectField(query_factory=lambda: Neighborhood.query.all(), get_label="name", allow_blank=False)
   type = QuerySelectField(query_factory=lambda: Gigtype.query.all(), get_label="name", allow_blank=False)
-  #date_entry = input('Enter a date (i.e. 2017,7,1)')
-  #year, month, day = map(int, date_entry.split(','))
-  #date = datetime(year, month, day) 
+  #date_entry = input("Enter a date i.e. 2019,11,29")
+  date_entry = '2019,11,29'
+  year, month, day = map(int, date_entry.split(','))
+  date = datetime.date(year, month, day) 
   submit = SubmitField(_l('Submit'))
 
 class SearchForm(FlaskForm):
