9a887bae34baf3ceae7279666c82d78e5084d958
dnilasor <rosalind26@gmail.com>
2019-12-06 21:06:07 -0800

    implemented gmail smtp server and fixed with SSL

--- app/auth/email.py
+++ app/auth/email.py
@@ -2,11 +2,11 @@ from flask import render_template, current_app
 from app.email import send_email
 from flask_babel import _, lazy_gettext as _l
 
-  
+
 def send_password_reset_email(user):
   token = user.get_password_reset_token()
   send_email(_('[QuickGig] Reset Your Password'),
-    sender=app.config['ADMINS'][0],
+    sender=current_app.config['ADMINS'][0],
     recipients=[user.email],
     text_body=render_template('email/password_reset.txt', user=user, token=token),
     html_body=render_template('email/password_reset.html', user=user, token=token))
